2cc8efb22d95264a4643ca88dcfdf1b9b95aa6d4
ST10448032 <ST10448032@rcconnect.edu.za>
2025-11-20 18:30:05 +0200

    Add files via upload

---
+++ LecturerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonthlyContractSystems.Models;
+using MonthlyContractSystems.Data;
+using System.Linq;
+
+namespace MonthlyContractSystems.Controllers
+{
+    public class LecturerController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Submit(ContractClaim claim)
+        {
+            claim.Id = ClaimRepository.Claims.Count + 1;
+            Console.WriteLine($"New claim created with Id={claim.Id}, Status={claim.Status}");
+
+            if (ModelState.IsValid)
+            {
+
+                claim.Id = ClaimRepository.Claims.Count + 1; // auto ID
+                claim.Status = "Pending";
+                ClaimRepository.Claims.Add(claim);
+                return RedirectToAction("History");
+                
+            }
+            return View(claim);
+        }
+
+        public IActionResult History()
+        {
+            var claims = ClaimRepository.Claims
+
+                .OrderByDescending(c => c.Month)
+                .ToList();
+
+            return View(claims);
+        }
+
+    }
+}
+
