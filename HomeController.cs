7fb4731c45a40102ebccaad04dec8c35f7f6cf63
ST10448032 <ST10448032@rcconnect.edu.za>
2025-11-20 18:29:30 +0200

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonthlyContractSystems.Data;
+using MonthlyContractSystems.Models;
+using System.Linq;
+
+namespace MonthlyContractSystems.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string role = "Lecturer")
+        {
+            ViewBag.Role = role;
+            var recentClaims = ClaimRepository.Claims
+                .OrderByDescending(c => c.Month)
+                .Take(5)
+                .ToList();
+
+            return View(recentClaims); // Pass as model instead of ViewBag
+        }
+    }
+}
